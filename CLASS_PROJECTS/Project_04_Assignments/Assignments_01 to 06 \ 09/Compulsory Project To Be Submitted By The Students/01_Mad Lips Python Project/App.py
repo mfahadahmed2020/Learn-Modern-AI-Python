f342191ff5ad8a1dcac0067520ab5c812f95bc47
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2025-04-07 02:36:47 +0500

    Add files via upload

---
+++ CLASS_PROJECTS/Project_04_Assignments/Assignments_01 to 06 \ 09/Compulsory Project To Be Submitted By The Students/01_Mad Lips Python Project/App.py
@@ -0,0 +1,23 @@
+print("Welcome To The Mad Lips Game")
+print("Imran Series Novel By Mazher Kaleem M.A.\n")
+
+hero = input("Ali Imran Msc Dsc Oxen")
+villain = input("Black Thunder")
+place = input("Cyberg Lab")
+adjective = input("MySterious")
+object_1 = input("Time Crtstal")
+action = input("Fight, Rescue, Destroy")
+ending = input("Triumphntly{Happily, Tragically}:")
+story = f"""
+Once Upon a Time, A Brave Hero Ali Imran Msc Dsc Oxen
+Was in Cyberg Lab.
+The Day Was MySterious, and Everyone Feared The Evil
+Black Thunder Holding The Powerfull Time Crystal,
+Black Thunder Planned To Rule The World.
+But Ali Imran Msc Dsc Oxen Had a Plan. With Courage, Ali Imran
+Decided to Fight After a Tough Battle, Black Thunder Was Defeated,
+and Cyberg Lab Was Saved.The Story Ends Happily & Tragically With
+Ali Imran Becoming A Legend.
+"""
+print("\nHere is Your Mad Lips Story:\n")
+print("Imran Series Novel By Mazher Kaleem M.A.")
