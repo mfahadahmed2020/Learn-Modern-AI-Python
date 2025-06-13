0438f6dae2f3bd9d204530af143d2e2fbc744ff6
M Fahad Ahmed <160360284+mfahadahmed2020@users.noreply.github.com>
2025-06-14 00:31:52 +0500

    Delete 00_Python_Colab/13_Traditional_OOP_Part_02 directory

--- 00_Python_Colab/13_Traditional_OOP_Part_02/oop.py
+++
@@ -1,7 +0,0 @@
-class Car:
-    def __init__(self, brand, color):
-        self.brand = brand
-        self.color = color
-
-    def display_info(self):
-        print(f"Brand: {self.brand}, Color: {self.color}")
